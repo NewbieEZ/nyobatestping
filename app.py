0a04c49a5f2d8d82c3ee2ffe6ada8c58f091535d
NewbieEZ <66250406+NewbieEZ@users.noreply.github.com>
2024-05-12 22:32:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 # Create a Flask application
 app = Flask(__name__)
 
 # Define a route for the root URL
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Usage: /add?a=<int>&b=<int> to add two integers a and b.'
+
+# Define a route for addition
+@app.route('/add')
+def add():
+    # Get the values of 'a' and 'b' from the query parameters
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    
+    # Perform addition
+    result = a + b
+    
+    # Return the result as a string
+    return 'The sum of {} and {} is {}.'.format(a, b, result)
 
 # Run the application if this file is executed
 if __name__ == '__main__':
